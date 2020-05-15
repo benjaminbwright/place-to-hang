c456ba3f1370bc02f3aca15b5ec6faa7b5fd0027
Benjamin Wright <benjamin@famousstick.com>
2020-05-15 00:39:24 -0400

    Added basic test for client side 404 route

--- server.js
+++ server.js
@@ -4,11 +4,15 @@ const app = express();
 const PORT = 4000;
 
 app.get("/api/stuff", (req, res) => {
-    res.json({
-        stuff: "Here is some stuff. Yo!!!"
-    })
-})
+  res.json({
+    stuff: "Here is some stuff. Yo!!!",
+  });
+});
+
+app.get("*", (req, res) => {
+  res.status(404).send("Page not found");
+});
 
 app.listen(PORT, () => {
-    console.log("The server be working.")
-})
+  console.log("The server be working.");
+});
---
+++ server.test.js
@@ -0,0 +1,15 @@
+  
+const axios = require("axios");
+
+describe("Server", () => {
+  it("should deliver a staus that is not 500 ", async () => {
+    try {
+      const { status } = await axios.get("/ben");
+      //console.log(status);
+      expect(status).not.toBe(undefined);
+    } catch (err) {
+      const { status } = err.response;
+      expect(status).not.toBe(undefined);
+    }
+  });
+});
