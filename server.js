1be7cc77ffa8832d50103d33e1ac6d12acd992c7
Benjamin Wright <benjamin@famousstick.com>
2020-05-17 18:57:40 -0400

    added path to client build folder

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require("express");
+const path = require("path");
+
 const app = express();
 
 const PORT = process.env.PORT || 4000;
 
-app.use(express.static("client/build"));
+app.use(express.static(path.join(__dirname, "client/build")));
+
+app.get("*", (req, res) => {
+  res.status(404).send("Page not found");
+});
 
 app.listen(PORT, () => {
   console.log("The server be working.");
